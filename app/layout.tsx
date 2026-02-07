38ff04c98ccf0eff996eb5f7e6a43614d8129793
artiveloper <artiveloper@gmail.com>
2026-02-07 23:53:40 +0900

    @vercel/analytics 을 추가한다.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Nanum_Myeongjo} from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const nanumMyeongjo = Nanum_Myeongjo({
     weight: ["400", "700", "800"],
@@ -137,6 +138,7 @@ export default function RootLayout({
         <html lang="ko" className={nanumMyeongjo.variable}>
         <body className="font-serif antialiased">
         {children}
+        <Analytics />
         <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{__html: JSON.stringify(jsonLd)}}
